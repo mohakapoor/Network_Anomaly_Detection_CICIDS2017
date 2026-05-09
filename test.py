7d5f732c2bb848d50eb9fcdcc30caeb8b106bfbe
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-05-09 14:51:21 +0530

    updated multiclass xgboost training scripts

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-from scapy.all import rdpcap
-packets = rdpcap("raw\Friday-WorkingHours.pcap")
-print(packets[0].time)  # unix timestamp
+import pandas as pd
+df = pd.read_parquet("unscaled_test.parquet")
+# one row per attack class
+sample_rows = df.groupby("Attack").head(1)
+
+print(sample_rows)
